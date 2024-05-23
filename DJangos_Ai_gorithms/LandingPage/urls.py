e3544face886586321770e7f1ed5e23df965891a
Jinit Vyas <108816742+JinitVyas@users.noreply.github.com>
2024-05-23 09:29:40 +0530

    Update urls.py

--- DJangos_Ai_gorithms/LandingPage/urls.py
+++ DJangos_Ai_gorithms/LandingPage/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('/', views.index, name="home"),
-    path('token_request',views.run_get_token),
-    path('find_Track',views.run_get_song_details),
-    path('recommend',views.run_get_recommendation),
-    path('features',views.run_get_features),
-    path('filterout',views.run_get_top10),
-    path('end',views.end)
+    path('/token_request',views.run_get_token),
+    path('/find_Track',views.run_get_song_details),
+    path('/recommend',views.run_get_recommendation),
+    path('/features',views.run_get_features),
+    path('/filterout',views.run_get_top10),
+    path('/end',views.end)
 ]
