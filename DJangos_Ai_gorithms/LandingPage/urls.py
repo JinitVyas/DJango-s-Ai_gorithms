91c17bf9c2a9b7a3d753efd2135eab9ad1b65814
Jinit Vyas <108816742+JinitVyas@users.noreply.github.com>
2024-05-26 09:42:06 +0530

    Update urls.py

--- DJangos_Ai_gorithms/LandingPage/urls.py
+++ DJangos_Ai_gorithms/LandingPage/urls.py
@@ -4,11 +4,11 @@ from LandingPage import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('/', views.index, name="home"),
-    path('/token_request',views.run_get_token),
-    path('/find_Track',views.run_get_song_details),
-    path('/recommend',views.run_get_recommendation),
-    path('/features',views.run_get_features),
-    path('/filterout',views.run_get_top10),
-    path('/end',views.end)
+    path('', views.index, name="home"),
+    path('token_request',views.run_get_token),
+    path('find_Track',views.run_get_song_details),
+    path('recommend',views.run_get_recommendation),
+    path('features',views.run_get_features),
+    path('filterout',views.run_get_top10),
+    path('end',views.end)
 ] 
